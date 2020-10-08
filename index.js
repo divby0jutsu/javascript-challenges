bede2affdb8aa6c2629f4c1b4ff5bbfefd19705f
Dine Mashlenko <outofdelorean@gmail.com>
2020-10-08 00:34:25 -0700

    JS basics challanges

---
+++ index.js
@@ -0,0 +1,67 @@
+// #1 Фильтрация строки
+
+// Создать переменную со строковым значением. Сформировать переменную которая будет состоять только из ГЛАСНЫХ букв этой строки. Результат вывести в консоль.
+
+const string = "Привет! Как дела?";
+
+let getVowels = (str) => {
+
+let stringArr = [...str];
+const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ю', 'я'];
+var vowelString=[];
+
+return vowelString = stringArr.filter(char => {
+   for (i=0; i<vowels.length; i++) {if (char == vowels[i]) {
+     console.log(char);
+     return char;
+  } } });
+  
+};
+
+console.log(getVowels(string));
+
+
+// #2 Выборка объекта
+
+// Сформировать произвольный массив объектов, описывающий, например, работников компании, в каждом объекте должны содержаться сл. свойства (поля) - name (содержащее имя сотрудника) и поле salary (содержащее зарплату сотрудника от 0 до 3000). Сформировать функцию в которую будет передан в качестве аргумента этот массив и возвращен из функции массив содержащий имена всех сотрудников, чья зарплата превышает 1000.
+
+const workers = [
+  {"name":"John","salary":500},
+  {"name":"Mike","salary":1300},
+  {"name":"Linda","salary":1500}];
+
+const getWorthyWorkers = employees => {
+
+
+const worthyEmployees = employees.filter(employee => employee.salary >     1000);
+return worthyEmployees;
+
+};
+
+console.log(getWorthyWorkers(workers));
+
+// #3 Анализ строки
+
+// Написать функцию которая будет принимать в качестве аргумента строку имитирующую адрес до файла. Функция должна возвращать true или false в зависимости от того есть у этого файла расширение html или нет.
+
+      
+const path = "/users/download/index";
+
+const isHtml = p => p.endsWith('.html');
+
+console.log(isHtml(path));
+     
+// #1 Фильтрация массива
+
+// Создайте функцию которая в качестве аргумента принимает целое число, и проверяет четное оно или нет. Затем создайте произвольный массив целых чисел. И используя функцию проверки числа на четность, создайте из готового массива новый, который будет содержать только четные числа. Результат выведите в консоль.
+
+      
+const mixedArray = [3,13,74,14,66,15,22,4];
+const isEven = num => Number.isInteger(num/2);
+
+const filterArray = (arr, func) => {
+  return arr.filter(num =>func(num) == true);
+}
+
+console.log(filterArray(mixedArray, isEven));
+    
